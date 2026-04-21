a9640469293ed5b0e9d6ae1eda8296efaba1a143
lil_maciek <f.maciej1998@gmail.com>
2026-04-21 13:43:22 +0200

    Dodanie komentarzy

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-LAMBDA = 5      # średnio 5 zgłoszeń na minutę
-H_AVG = 3       # średni czas trwania rozmowy w minutach
-T_TOTAL = 60    # czas trwania symulacji (np. 1 godzina) [cite: 23]
+LAMBDA = 6     # średnio 5 zgłoszeń
+H_AVG = 3       # średni czas trwania rozmowy
+T_TOTAL = 60    # czas trwania symulacji
 
 import numpy as np
 
@@ -11,17 +11,14 @@ import numpy as np
 def generuj_ruch(lambda_param, h_sredni, t_symulacji):
     momenty_przyjscia = []
     aktualny_czas = 0
-    while aktualny_czas < t_symulacji:
+    while aktualny_czas < t_symulacji: #Generowanie momentów przyjścia połączeń
         odstep = np.random.exponential(1 / lambda_param)
         aktualny_czas += odstep
         if aktualny_czas < t_symulacji:
             momenty_przyjscia.append(aktualny_czas)
 
-    # 2. Losujemy czas trwania każdej rozmowy
-    czasy_trwania = np.random.exponential(h_sredni, len(momenty_przyjscia))
-
-    # 3. Wyznaczamy kiedy połączenia się kończą
-    momenty_zakonczenia = [p + t for p, t in zip(momenty_przyjscia, czasy_trwania)]
+    czasy_trwania = np.random.exponential(h_sredni, len(momenty_przyjscia)) #Losowanie czasu trwania rozmów
+    momenty_zakonczenia = [p + t for p, t in zip(momenty_przyjscia, czasy_trwania)] #Wyznaczanie momentów zakończenia
 
     return momenty_przyjscia, momenty_zakonczenia
 
